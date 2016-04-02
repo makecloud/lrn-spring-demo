c57685178b27f7512a862fa4a1e9adff4ef1da09
liuyihui <makecloud@qq.com>
2016-04-02 15:37:07 +0800

    first commit lrn-spring-demo within eclipse by liuyihui

---
+++ src/main/java/com/liuyihui/springdemo/model01/Greeter.java
@@ -0,0 +1,7 @@
+package com.liuyihui.springdemo.model01;
+
+public class Greeter {
+	public String sayHello(){
+		return "hello world";
+	}
+}
---
+++ src/main/java/com/liuyihui/springdemo/model01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.liuyihui.springdemo.model01;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println(currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+}
